c34a1d0be8d886f15ff9d0c4c1b79c10e425be8c
R1tterLanze <bt709436@uni-bayreuth.de>
2022-05-26 15:05:22 +0200

    Implemented fetch and object usage

---
+++ hbonds_search_pymol/hb-search_script.py
@@ -0,0 +1,232 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # GUI hbond
+
+# ### Initialization
+
+# In[52]:
+
+
+import pandas as pd
+import subprocess
+import os
+from pymol import cmd, stored
+import platform
+from typing import List
+
+
+# ### Functions
+
+# In[53]:
+
+
+"""
+def form(pdbstr, x):
+    '''
+    Part of lambda function to format dataframe to Pymol compatible form:
+    from "A:183:LEU:O" to "/2akr/A/A/LEU`183/O"
+    :param pdbstr: pdb code of handled structure
+    :param x: entry within dataframe
+    '''
+    temp = x.split(':')
+    x = f'/{pdbstr}//{temp[0]}/{temp[2]}/{temp[3]}'
+    return x
+
+def pymol_display(df):
+    '''
+    '''
+    zilis = list(zip(df['ACC'].tolist(), df['DONO'].tolist()))
+    for i in zilis:
+        cmd.distance( i[0] , i[1])
+    
+def hbsearch(pdbstr:str) -> pd.DataFrame():
+    '''
+    Executing hb_search with set parameters and extract HBOND-entries from output
+    :return df_hbond: Dataframe with all HBOND entries from hb_search output 
+    '''
+    
+    HEAD_LST = ['IDENT','ACC','sep1','DONO',':','x','y','z','sep2','a','b']
+    
+    # Setting environment variable
+    os.environ['PSE_FILE'] = 'period-table-info.txt'
+    
+    # Executing hb_search
+    hbs = subprocess.run(f"./hb-search -hb hb-define.txt {pdbstr}.pdb", 
+                         stdout=subprocess.PIPE, shell=True, check=True, text=True)
+    
+    # Decode and format hb_search output
+    hbs_hbb = [i for i in hbs.stdout.split('\n') if i[0:5] == "HBOND"]
+    hbs_splt = [i.split() for i in hbs_hbb]
+
+    # Return dataframe
+    df_hbond = pd.DataFrame(hbs_splt, columns = HEAD_LST)
+    
+    df_hbond = df_hbond[['ACC', 'DONO']]
+    df_hbond['ACC'] = df_hbond['ACC'].map(lambda x: form(pdbstr,x) )
+    df_hbond['DONO'] = df_hbond['DONO'].map(lambda x: form(pdbstr,x) )
+    
+    pymol_display(df_hbond)
+    
+    return df_hbond
+
+#cmd.extend('hbsearch', hbsearch)"""
+
+
+# In[54]:
+
+
+def changeDirectory(programDirectory: str = "."):
+    
+    cmd.cd(os.path.normpath(programDirectory))
+    #os.chdir(os.path.normpath(programDirectory)) #can be deleted?
+    # Usefull if we want to give an error! To tell the person in which directory they are located!
+    #cwd = os.getcwd()
+
+
+# In[55]:
+
+
+def useObject(input_molecule: str):
+    
+    cmd.save(os.path.normpath(f"./pdb_files/{input_molecule}.pdb"), input_molecule)
+
+
+# In[69]:
+
+
+def removeObject(input_molecule: str):
+    
+    os.remove(os.path.normpath(f"./pdb_files/{input_molecule}.pdb"))
+
+
+# In[57]:
+
+
+def fetch(pdbID: str, object_name: str = ""):
+    if object_name == "":
+        object_name = pdbID
+        
+    cmd.cd(os.path.normpath("./pdb_files/"))
+    cmd.fetch(pdbID, type= "pdb", name = object_name)
+    cmd.cd(os.path.normpath(".."))
+
+
+# In[58]:
+
+
+def startHBsearch(molecule: str, hb_file: str = "hb-define.txt", solvent_key:str = "NONE", pse_file:str ="period-table-info.txt", connections: str = "0" ):
+
+    # Setting environment variable
+    os.environ['PSE_FILE'] = pse_file
+    # Determine operation system
+    system = platform.system()
+    # Executing hb_search
+    hbs_output = subprocess.run(os.path.normpath(f"./{system}/hb-search -hb {hb_file} -solv {solvent_key} -con {connections} ./pdb_files/{molecule}.pdb"), capture_output=True, shell=True, check = True, text = True).stdout
+    return hbs_output
+
+
+# In[59]:
+
+
+def readInHBS(hbs_output: str):
+    hbs_rows = [i for i in hbs_output.split('\n')]
+    hbs_split = [i.split() for i in hbs_rows]
+    
+    HEAD_LST = ['IDENT','ACC','sep1','DONO',':','x','y','z','sep2','a','b']
+    
+    df = pd.DataFrame(hbs_split, columns = HEAD_LST)
+    df = df[df["IDENT"] == "HBOND"]
+    return df
+
+
+# In[60]:
+
+
+def prepareLists(dataframe: pd.DataFrame):
+    
+    acceptor_pre = list(dataframe["ACC"])
+    donor_pre = list(dataframe ["DONO"])
+    
+    acceptor = []
+    donor = []
+    
+    for i in range(len(acceptor_pre)):#modify with zip
+        acceptor.append(tuple(acceptor_pre[i].split(":")))
+        
+    for j in range(len(donor_pre)):
+        donor.append(tuple(donor_pre[j].split(":")))
+
+
+    
+    return acceptor, donor
+
+
+# In[64]:
+
+
+def displayDistances(acceptor: List, donor: List, object_name: str):
+    
+    bondList = []
+    
+    for i in range(len(acceptor)): #modify with zip
+        print(acceptor[i], donor[i])
+        cmd.distance(f"{object_name}_hydrogenBond_{i}", 
+                     f"{object_name}//{acceptor[i][0]}/{acceptor[i][1]}/{acceptor[i][3]}", 
+                     f"{object_name}//{donor[i][0]}/{donor[i][1]}/{donor[i][3]}", )
+
+        bondList.append(f"{object_name}_hydrogenBond_{i}")
+    cmd.group(f"{object_name}_hydrogenBonds", " ".join(bondList))
+    cmd.hide("labels", f"{object_name}_HydrogenBonds")
+
+
+# In[68]:
+
+
+def main(molecule:str, molecule_name = "", directory:str = ".", use_object: str = "0", remove_object = "0", hb_file: str = "hb-define.txt", solvent_key:str = "NONE", pse_file:str ="period-table-info.txt", connections: str = "0"):
+    changeDirectory(directory)
+    
+    print(molecule_name)
+    if use_object == "0":
+        fetch(molecule, molecule_name)
+    elif use_object == "1":
+        useObject(molecule)
+        
+    hbs_output = startHBsearch(molecule, hb_file, solvent_key, pse_file, connections)
+    hbs_dataframe = readInHBS(hbs_output)
+    acceptor, donor = prepareLists(hbs_dataframe)
+    
+    if molecule_name == "":
+        displayDistances(acceptor, donor, molecule)
+    else:
+        displayDistances(acceptor, donor, molecule_name)
+    
+    if remove_object == "1":
+        removeObject(molecule)
+
+
+# In[67]:
+
+
+cmd.extend("hbsearch", main)
+
+
+# In[ ]:
+
+
+# hbsearch 
+
+
+# ### Main body
+
+# In[34]:
+
+
+
+
+
+# In[35]:
+
+
+
+
